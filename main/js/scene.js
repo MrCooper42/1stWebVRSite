4ed817431215f3af7f1f94b481df60fcb34a7057
MrCooper42 <mr.cooper42co@gmail.com>
2016-08-31 22:19:16 -0600

    links a dead end

--- main/js/scene.js
+++ main/js/scene.js
@@ -1,11 +1,11 @@
-$(#comp_link)('click', function () {
-  window.location.href = './sceneCom.html';
+$(#comp_link).click(function () {
+  window.location.href = './sceneComp.html';
 });
 
-$(#phone_link)('click', function () {
+$(#phone_link).click(function () {
   window.location.href = './scenePhone.html';
 });
 
-$(#vive_link)('click', function () {
+$(#vive_link).click(function () {
   window.location.href = './sceneVive.html';
 });
