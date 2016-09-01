e99b4f22444a762f57411199873b7eb748e7f207
MrCooper42 <mr.cooper42co@gmail.com>
2016-08-31 19:40:16 -0600

    adding links

---
+++ main/js/scene.js
@@ -0,0 +1,11 @@
+$(#comp_link)('click', function () {
+  window.location.href = './sceneCom.html';
+});
+
+$(#phone_link)('click', function () {
+  window.location.href = './scenePhone.html';
+});
+
+$(#vive_link)('click', function () {
+  window.location.href = './sceneVive.html';
+});
