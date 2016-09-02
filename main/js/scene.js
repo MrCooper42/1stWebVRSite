a3c4df910faae566452ee4373e4b02941f7776e0
MrCooper42 <mr.cooper42co@gmail.com>
2016-09-02 01:19:18 -0600

    css and html fixed photos of game added

--- main/js/scene.js
+++ main/js/scene.js
@@ -1,11 +1,12 @@
-$(#comp_link).click(function () {
-  window.location.href = './sceneComp.html';
-});
-
-$(#phone_link).click(function () {
-  window.location.href = './scenePhone.html';
-});
-
-$(#vive_link).click(function () {
-  window.location.href = './sceneVive.html';
-});
+// $(#comp_link).click(function () {
+//   window.location.href = './sceneComp.html';
+// });
+//
+// $(#phone_link).click(function () {
+//   window.location.href = './scenePhone.html';
+// });
+//
+// $(#vive_link).click(function () {
+//   window.location.href = './sceneVive.html';
+// });
+// patch will be coming soon to allow links
